16e466733f1147ae1257e673d88e613e48427c13
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-02 10:16:12 +0200

    code to copy pdf

---
+++ copy_tex.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# copy pdfs and name correctly
+# cp -p ms-overleaf-rose/main.tex ms_kleptomove_`date -I`.tex
+cp -p ms-overleaf-rose/main.pdf ms_prelim_rose_proposal_PRG_2021_`date -I`.pdf
