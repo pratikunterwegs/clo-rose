6e0705771a054cb16ea78734833129fb4de2a219
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-26 15:59:56 +0200

    add species filter

--- scripts/00_filter_with_auk.jl
+++ scripts/00_filter_with_auk.jl
@@ -1,6 +1,32 @@
 # use auk via RCall to filter ebird data
 
 using RCall
+using XLSX
+using DataFrames
+# species of interest
+# read data from soib
+soib = DataFrame(XLSX.readtable("data/2020-state-india-birds-trait-dat.xlsx", "Information")...)
+rename!(soib, replace.(names(soib), r" \((India Checklist)\)" => ""))
+rename!(soib, replace.(names(soib), " " => "_"))
+rename!(soib, lowercase.(names(soib)))
+
+# select all migratory birds then all landbirds
+filter!(row -> (occursin(r"(Migratory)", row.migratory_status)), soib)
+
+# print all migratory birds
+print(soib.common_name)
+
+# select landbirds of interest -- migratory from europe
+soi_query = r"(Cuckoo)|(Roller)|(Wryneck)|(Shrike)|(Lark)|(Flycatcher)|(Warbler)|(Flycatcher)|(throat)|(Thrush)|(chat)|(Wheatear)|(Accentor)|(Wagtail)|(Pipit)|(Rosefinch)|(Bunting)"
+
+filter!(row -> (occursin(soi_query, row.common_name)), soib)
+
+# save and classify manually
+CSV.write("data/data_migratory_landbirds_soib.csv", soib)
+
+# remove birds that dont' migrate from europe
+soi_ief_query = r"(Drongo)|(Cuckooshrike)|(Lesser Cuckoo)|(Himalayan Cuckoo)|(Grey-backed Shrike)|(Sykes\'s Short-toed)|(Brook)|(Tytler)|(Sulphur-bellied)|(Tickell)|(Smoky)|(Green-crowned)|(Large-billed Leaf)|(Western Crowned)|(Thick-billed)|(Dark-sided)|(Brown-breasted)|(Blue-throated)|(Large Blue)|(Verditer)|(Rubythroat)|(Ultramarine)|(Rusty-tailed)|(Taiga)|(Kashmir)|(Blue-capped)|(Hodgson\'s Bushchat)"
+
 R"library(auk)
 
   # prepare filters
