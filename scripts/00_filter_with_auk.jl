2074648fa7d07c64728a5d69da51e54de7f66815
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-27 13:55:39 +0200

    get ief data

--- scripts/00_filter_with_auk.jl
+++ scripts/00_filter_with_auk.jl
@@ -9,29 +9,28 @@ soib = CSV.read("data/data_migratory_landbirds_soib.csv", DataFrame)
 
 R"library(auk)"
 
-# WORK IN PROGRESS
-
 # get species of interest
-for flyway in unique(soib.flyway)
-  soi = filter(row -> row.flyway == flyway, soib)
+flyways = ["ief"]
+for flyway in flyways
+  soi = filter(row -> occursin(flyway, row.flyway), soib)
   soi = soi.scientific_name
+
+  path = string("data/output/data_ebird_", flyway, ".txt")
 
   R"
   # prepare filters
-  file_ebird = file.path('data/observations/ebd_IN_relSep-2020.txt')
+  file_ebird = file.path('data/ebd_IN_relSep-2020.txt')
   ebd_filters = auk_ebd(file = file_ebird) %>% 
     auk_species($soi) %>%
     auk_country(country = 'IN') %>% 
     auk_date(c('2015-01-01', '2019-12-31'))
 
   # run filters
-  file_output = 'data/output/data_ebird.txt'
+  file_output = $path
   ebd_filtered = auk_filter(
     ebd_filters,
     file = file_output,
     overwrite = FALSE
   )"
-
-
 end
 
