1bbc66538ecf31d773af7987dc302f7640ee083c
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-30 16:00:03 +0200

    code to count

--- scripts/00_filter_soib.jl
+++ scripts/00_filter_soib.jl
@@ -16,6 +16,16 @@ filter!(row -> (occursin(r"(Migratory)", row.migratory_status)), soib)
 # print all migratory birds
 print(soib.common_name)
 
+# count migratory status
+combine(groupby(soib, :migratory_status),
+    nrow => :count)
+
+# check local migrants
+migrant_landbirds = filter(row -> (row.migratory_status == "Migratory-Local") &&
+     (!occursin(r"(Goose)|(duck)|(anser)|(piper)|(Gull)|(Plover)|(tern)", 
+     row.common_name)), soib)
+print(migrant_landbirds.common_name)
+
 # select landbirds of interest by common name
 soi_query = r"(Cuckoo)|(Roller)|(Wryneck)|(Shrike)|(Lark)|(Flycatcher)|(Warbler)|(Flycatcher)|(throat)|(Thrush)|(chat)|(Wheatear)|(Accentor)|(Wagtail)|(Pipit)|(Rosefinch)|(Bunting)"
 
